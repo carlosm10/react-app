5a0a63e1e7d68f645872c08abf7351fd0f3a82c8
Carlos Mondragon <carlos.mondragon@epicor.com>
2025-10-20 15:29:22 -0600

    add tags and meta to html

--- src/App.js
+++ src/App.js
@@ -3,19 +3,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <nav className="App-header">
+          <div className="App-link">
+            <a           
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">Logo</a>
+          </div>
+          <div className="App-link">
+            <a           
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">Home</a>
+          </div>
+          <div className="App-link">
+            <a           className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">About</a>
+          </div>
+          <div className="App-link">
+            <a           className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">Order</a>
+          </div>
+          <div className="App-link">
+            <a           className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">Contact</a>
+          </div>
+        </nav>
       </header>
+      <main className="App-main">
+        <p>Some text</p>
+      </main>
+      <footer className="App-footer"><p>Some footer</p></footer>
     </div>
   );
 }
